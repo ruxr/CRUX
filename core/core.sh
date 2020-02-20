36b550c7ae257bab86c2c56509fe287a3ad0125b
Roman Oreshnikov <r.oreshnikov@gmail.com>
2020-02-20 15:51:23 +0400

    Default boot kernel optimization

--- core/core.sh
+++ core/core.sh
@@ -1,22 +1,12 @@
 #!/bin/sh
-exec >&2
-E() { echo Installation aborted!; exit 1; }
-Q() { read -p "$*? [Y/n] " A; case $A in ""|[yY]);; *) exit;; esac; }
-[ $# = 0 ] && Q Install kernel #REL# to /
-/bin/sed '1,/^exit/d' $0 | /bin/tar -C ${1:-/} -xJf - || E
+/bin/sed '1,/^exit/d' $0 | /bin/tar -C ${1:-/} -xJf - || exit
+B=$1/boot/
 V=$(/bin/sed 's|^Linux version \([^ ]*\).*|\1|' /proc/version)
-[ $# = 0 ] && Q Set the default kernel as #REL#
 for F in vmlinuz initrd System.map config; do
-	F=$1/boot/$F
-	[ "x$V" = "x#REL#" ] || [ ! -f $F-$V ] || /bin/ln -f $F-$V $F-old || E
-	/bin/ln -f $F-#REL# $F || E
+	/bin/ln -f $B$F-#REL# $B$F
 done
-[ $# = 0 ] && Q Remove unused kernels
-F=$1/boot/config-old
-[ -f $F ] && V=$(/bin/sed 's|^# Linux/x86 \([^ ]*\).*|\1|p;d' $F)
 for F in $1/lib/modules/*; do
 	case " $V #REL# " in *\ ${F##*/}\ *) continue;; esac
-	/bin/rm -rf $F $1/boot/*-${F##*/}
+	/bin/rm -rf $F $B*-${F##*/}
 done
-[ $# = 0 ] || exec /bin/rm $0
 exit
