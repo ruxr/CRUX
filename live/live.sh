f3dadc824acff27d014f8c4768ccbd9b30f07565
Roman Oreshnikov <r.oreshnikov@gmail.com>
2022-04-19 19:18:44 +0400

    Build reorganization for port "live"

--- live/live.sh
+++ live/live.sh
@@ -4,20 +4,20 @@ E() { echo Terminated by error!; exit 1; }
 Q() { read -p "$*? [Y/n] " A; case $A in ""|[yY]);; *) exit;; esac; }
 [ -n "$1" ] && N=$1 || N=/mnt
 Q Use $N as the root of the temporary system
-/bin/mkdir -p $N || E
-/bin/mount -t tmpfs none $N || E
-/bin/sed "1,/^exit/d" $0 | /bin/tar -C $N -xJf - || E
-/bin/cp /etc/ssh/ssh_host* $N/etc/ssh || E
-/bin/cp /etc/group /etc/hosts /etc/host.conf /etc/inputrc /etc/issue \
+mkdir -p $N || E
+mount -t tmpfs none $N || E
+sed '1,/^exit/d' $0 | tar -C $N -xJf - || E
+cp /etc/ssh/ssh_host* $N/etc/ssh || E
+cp /etc/group /etc/hosts /etc/host.conf /etc/inputrc /etc/issue \
 	/etc/login.defs /etc/motd /etc/nsswitch.conf /etc/passwd \
 	/etc/resolv.conf /etc/rpc /etc/securetty /etc/shadow $N/etc || E
-/bin/sed -i "s%/bin/bash%/bin/sh%" $N/etc/passwd || E
-for M in /dev /dev/pts /dev/shm /proc /run /sys /tmp; do
-        /bin/mountpoint -q $M && /bin/mount --bind $M $N$M || E
+sed -i 's%/bin/bash%/bin/sh%' $N/etc/passwd || E
+for M in /dev /proc /run /sys /tmp; do
+        /bin/mountpoint -q $M && /bin/mount --rbind $M $N$M || E
 done
 Q Switch to temporary system in $N
-T=$(/usr/bin/tty)
+T=$(tty)
 cd $N
-/sbin/pivot_root . mnt || E
-exec /usr/bin/chroot . /sbin/init S <.$T >.$T 2>&1 || E
+pivot_root . mnt || E
+exec chroot . /sbin/init S <.$T >.$T 2>&1 || E
 exit
